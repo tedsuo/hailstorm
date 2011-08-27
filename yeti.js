6be048f94720e26cae83d9375b91f2b533e93af5
Micah Lee <micahflee@gmail.com>
2011-08-26 18:28:59 -0700

    yeti can receive commands

--- yeti.js
+++ yeti.js
@@ -1,9 +1,32 @@
 var http = require('http');
+var express = require('express');
 
-var app = http.createServer(function (req, res) {
-res.writeHead(200, { 'Content-Type': 'text/html' });
-res.end('<html><title>you have encountered a hailstorm yeti</title><style>body {text-align:center; background-color:#000000; color:#cc0000;}</style><head><head><body><h1>you have encountered a HAILSTORM yeti</h1><img src="http://mysticinvestigations.com/supernatural/wp-content/uploads/2011/01/yeti-snowman.jpg"></body></html>');
+var app = express.createServer();
+
+// in case people are curious what a yeti is
+app.get('/', function(req, res) {
+        res.send('<html><title>you have encountered a hailstorm yeti</title><style>body {text-align:center; background-color:#000000; color:#cc0000;}</style><head><head><body><h1>you have encountered a HAILSTORM yeti</h1><img src="http://mysticinvestigations.com/supernatural/wp-content/uploads/2011/01/yeti-snowman.jpg"></body></html>');
 });
 
-app.listen(parseInt(process.env.PORT) || 1337);
+// yeti's settings
+app.post('/settings', function(req, res) {
+    res.send('');
+});
+
+// start
+app.post('/start', function(req, res) {
+    res.send('');
+});
+
+// stop
+app.post('/stop', function(req, res) {
+    res.send('');
+});
+
+// status
+app.get('/status', function(req, res) {
+    res.send('');
+});
+
+app.listen(parseInt(process.env.YETI_PORT) || 1337);
 console.log('Listening on ' + app.address().port);
