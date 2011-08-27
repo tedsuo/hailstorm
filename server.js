97ca0f57d5184003a5d357835f412005b721ba35
cooperq <cooper@radicaldesigns.org>
2011-08-26 18:08:01 -0700

    fruity oaty bars

--- server.js
+++ server.js
@@ -3,15 +3,7 @@ var nko = require('nko')('sOlBQnEkup4F/fL4');
 
 var app = http.createServer(function (req, res) {
 res.writeHead(200, { 'Content-Type': 'text/html' });
-res.end('\
-<pre>\
- __  __     ______     __     __         ______     ______   ______     ______     __    __   \n \
-/\ \_\ \   /\  __ \   /\ \   /\ \       /\  ___\   /\__  _\ /\  __ \   /\  == \   /\ "-./  \   \n\
-\ \  __ \  \ \  __ \  \ \ \  \ \ \____  \ \___  \  \/_/\ \/ \ \ \/\ \  \ \  __<   \ \ \-./\ \  \n\
- \ \_\ \_\  \ \_\ \_\  \ \_\  \ \_____\  \/\_____\    \ \_\  \ \_____\  \ \_\ \_\  \ \_\ \ \_\ \n\
-  \/_/\/_/   \/_/\/_/   \/_/   \/_____/   \/_____/     \/_/   \/_____/   \/_/ /_/   \/_/  \/_/ \n\
-</pre>\
-');
+res.end('Get ready for the hailstorm! >:) ');
 });
 
 app.listen(parseInt(process.env.PORT) || 7777);
