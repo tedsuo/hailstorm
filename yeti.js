caa43ff5d9e8c7a8bed016178ec30ca477a2166e
Micah Lee <micahflee@gmail.com>
2011-08-27 00:01:03 -0700

    yeti now keeps track of status

--- yeti.js
+++ yeti.js
@@ -5,6 +5,7 @@ var express = require('express');
 // settings for this yeti
 var settings = {};
 var request_log = {};
+var requests_sent = 0;
 var agent;
 
 // returns http or https
@@ -22,7 +23,7 @@ app.use(express.logger({ format: ':method :url' }));
 
 // in case people are curious what a yeti is
 app.get('/', function(req, res) {
-        res.send('<html><title>you have encountered a hailstorm yeti</title><style>body {text-align:center; background-color:#000000; color:#cc0000;}</style><head><head><body><h1>you have encountered a HAILSTORM yeti</h1><img src="http://mysticinvestigations.com/supernatural/wp-content/uploads/2011/01/yeti-snowman.jpg"></body></html>');
+    res.send('<html><title>you have encountered a hailstorm yeti</title><style>body {text-align:center; background-color:#000000; color:#cc0000;}</style><head><head><body><h1>you have encountered a HAILSTORM yeti</h1><img src="http://mysticinvestigations.com/supernatural/wp-content/uploads/2011/01/yeti-snowman.jpg"></body></html>');
 });
 
 // yeti's settings
@@ -31,21 +32,20 @@ app.post('/settings', function(req, res) {
     stop(); // hammer time
 
     settings = req.body;
-    settings.firing = false;
-    
-    console.log('recieved settings');
-    console.log(settings);
+    settings.status = 'awaiting commands';
 
     get_protocol().globalAgent.maxSockets = settings.concurrency;
 
-    res.send('ready to fire');
+    console.log(settings);
+    res.send(settings.status);
 });
 
 // start
 app.post('/start', function(req, res) {
-    console.log('firing!');
-    settings.firing = true;
+    settings.status = 'attacking';
     var num_requests = 0;
+    request_log = [];
+    requests_sent = 0;
     while(num_requests < settings.max_requests) {
         for(var i=0; i<settings.requests.length; i++) {
             console.log('sending request '+num_requests+': '+JSON.stringify(settings.requests[i]));
@@ -73,6 +73,7 @@ app.post('/start', function(req, res) {
                 request_log[this.request_id].response_time = request_log[this.request_id].end_time - request_log[this.request_id].start_time;
                 request_log[this.request_id].status_code = res.statusCode;
                 console.log('request '+this.request_id+' '+request_log[this.request_id].method+' '+request_log[this.request_id].path+' finished with code '+request_log[this.request_id].status_code+' in '+request_log[this.request_id].response_time+' ms');
+                requests_sent++;
             });
             req.request_id = num_requests;
             req.on('error', function(e){
@@ -89,40 +90,34 @@ app.post('/start', function(req, res) {
             if(num_requests == settings.max_requests) break;
         }
     }
-    res.send('firing!');
+    console.log(settings.status);
+    res.send(settings.status);
 });
 
 // stop
 function stop() {
-    settings.firing = false;
-    if(agent)
-        agent.queue = [];
+    settings.status = 'hibernating';
+    if(agent) agent.queue = [];
 }
 app.post('/stop', function(req, res) {
     stop();
-    res.send('this yeti has put down its snowballs');
+    res.send(settings.status);
 });
 
 // status
 app.get('/status', function(req, res) {
-    // status
-    if(settings.firing) {
-        if(agent && agent.queue && agent.queue.length == 0)
-            settings.firing = false;
-    }
-    var status;
-    if(settings.firing) {
-        status = 'attacking';
-    } else {
-        status = 'hibernating';
+    if(settings.max_requests == requests_sent) {
+        settings.status = 'catching breath';
     }
 
-    res.send({
-        status: status,
-        statistics: {
-            
-        }       
-    });
+    if(settings.status == 'attacking') { 
+        res.send({
+            status: settings.status,
+            requests_sent: requests_sent
+        });
+    } else {
+        
+    }
 });
 
 var port = parseInt(process.env.YETI_PORT) || 1337;
