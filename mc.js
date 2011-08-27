bb47d1bad852cbe260e068c2d24a03c6b191deb0
William Budington <bill.budington@gmail.com>
2011-08-27 12:14:39 -0700

    Bugfixes to yeti, getting dnode responses in MC

--- mc.js
+++ mc.js
@@ -1,11 +1,22 @@
 var http = require('http');
+var dnode = require('dnode');
 var express = require('express');
 
 var app = express.createServer();
 app.use(express.bodyParser());
 
-function set_charge_orders(req, res, charge_statuses){
-  var yettis = req.body.yettis;
+var yettis = {};
+
+var dserver = dnode(function (client, conn){
+  this.callIn = function(){
+    yettis[conn.id] = {client: client, conn: conn};
+    conn.on('end', function(){
+      delete yettis[conn.id];
+    });
+  };
+}).listen(1337);
+
+function set_charge_orders(req, res){
   var target = req.body.target;
   var individual_concurrency = Math.floor(req.body.concurrency / yettis.length);
   var max_requests = req.body.max_requests;
@@ -14,34 +25,16 @@ function set_charge_orders(req, res, charge_statuses){
   var target_json = JSON.stringify(target);
   var res_array = [];
   for(i in yettis){
-    var options = {
-      host: yettis[i],
-      port: 80,
-      method: 'GET',
-      path: '/'
-    };
-    var yettireq = http.request(options, function(resp){
-      if(resp.statusCode != "200"){
-        res_array.push(yettis[this.request_id] + " response: " + resp.statusCode);
-      } else {
-        res_array.push(yettis[this.request_id] + " non-200 response: " + resp.statusCode);
-      }/*
-      resp.request_id = this.request_id;
-      resp.on('data', function (chunk) {
-        console.log('BODY: ' + chunk);
-      });*/
-      if(res_array.length == yettis.length){
-        res.send(res_array.join("<br />"));
-      }
-    });
-    yettireq.request_id = i;
-    yettireq.end();
+    console.log('yeti');
+    yettis[i].client.set(target_json, function(err, status){
+      console.log(status);
+    }); 
   }
+  res.send('test');
 }
 
 app.post('/set_charge_orders', function(req, res){
-  var charge_statuses = {};
-  set_charge_orders(req, res, charge_statuses, charge_statuses);
+  set_charge_orders(req, res);
 });
 
 app.listen(parseInt(process.env.MC_PORT) || 31337, '127.0.0.1');
