e960f07d119ce2b799b7e6ef4a4b3c24b162840e
cooperq <cooper@radicaldesigns.org>
2011-08-27 13:21:27 -0700

    adding file for oauth

--- ui/server.js
+++ ui/server.js
@@ -1,6 +1,28 @@
 var express = require('express');
 var controller = require('./controller');
 var model = require('./model');
+var everyauth = require('everyauth');
+// Step 1 code goes here
+
+everyauth.google
+  .appId('YOUR CLIENT ID HERE')
+  .appSecret('YOUR CLIENT SECRET HERE')
+  .scope('https://www.google.com/m8/feeds') // What you want access to
+  .handleAuthCallbackError( function (req, res) {
+    // If a user denies your app, Google will redirect the user to
+    // /auth/facebook/callback?error=access_denied
+    // This configurable route handler defines how you want to respond to
+    // that.
+    // If you do not configure this, everyauth renders a default fallback
+    // view notifying the user that their authentication failed and why.
+  })
+  .findOrCreateUser( function (session, accessToken, accessTokenExtra, googleUserMetadata) {
+    // find or create user logic goes here
+    // Return a user or Promise that promises a user
+    // Promises are created via
+    //     var promise = this.Promise();
+  })
+  .redirectPath('/');
 
 if(process.env.NODE_ENV == 'production'){
   port = 80;
@@ -12,9 +34,11 @@ var app = express.createServer(
   express.static(__dirname + '/public'),
   express.cookieParser(),
   express.session({ secret: "magicpants" }),
+  everyauth.middleware(),
   model.setUser()
 );
 
+everyauth.helpExpress(app);
 app.set('view engine', 'ejs');
 
 app.set('view options', {
