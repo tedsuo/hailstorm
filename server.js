2deb96486ad0a88448cd52e87ab21993f33d8031
cooperq <cooper@radicaldesigns.org>
2011-08-26 17:49:38 -0700

    first commit of hailstorm

---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var nko = require('nko')('sOlBQnEkup4F/fL4');
+
+var app = http.createServer(function (req, res) {
+res.writeHead(200, { 'Content-Type': 'text/html' });
+res.end('
+ __  __     ______     __     __         ______     ______   ______     ______     __    __    
+/\ \_\ \   /\  __ \   /\ \   /\ \       /\  ___\   /\__  _\ /\  __ \   /\  == \   /\ "-./  \   
+\ \  __ \  \ \  __ \  \ \ \  \ \ \____  \ \___  \  \/_/\ \/ \ \ \/\ \  \ \  __<   \ \ \-./\ \  
+ \ \_\ \_\  \ \_\ \_\  \ \_\  \ \_____\  \/\_____\    \ \_\  \ \_____\  \ \_\ \_\  \ \_\ \ \_\ 
+  \/_/\/_/   \/_/\/_/   \/_/   \/_____/   \/_____/     \/_/   \/_____/   \/_/ /_/   \/_/  \/_/ 
+');
+});
+
+app.listen(parseInt(process.env.PORT) || 7777);
+console.log('Listening on ' + app.address().port);
