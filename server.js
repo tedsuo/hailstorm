3638f6f12646da1c75686fd64dca62e11e3a5b19
Micah Lee <micah@eff.org>
2011-08-28 16:24:45 -0700

    trying some fucked up hack

--- server.js
+++ server.js
@@ -19,18 +19,27 @@ if(process.env.CLOUD_SERVER == 'true') {
 } else {
 
   var nko = require('nko')('sOlBQnEkup4F/fL4');
+  var node4 = '/opt/node-0.4.11/bin/node';
+  var node5 = '/opt/node-0.5.5/bin/node';
 
   // spawn the UI
-  var ui = spawn('node', ['server.js'], { cwd: __dirname + '/ui/' });
+  var ui = spawn(node4, ['server.js'], { cwd: __dirname + '/ui/' });
   ui.stdout.on('data', function(data){ log('UI stdout', data); });
   ui.stderr.on('data', function(data){ log('UI stderr', data); });
   ui.on('exit', function(code){ console.log('UI exited with code '+code); });
   console.log('UI has been spawned');
 
   // spawn the MC
-  var mc = spawn('node', ['mc.js'], { cwd: __dirname });
+  var mc = spawn(node4, ['mc.js'], { cwd: __dirname });
   mc.stdout.on('data', function(data){ log('MC stdout', data); });
   mc.stderr.on('data', function(data){ log('MC stderr', data); });
   mc.on('exit', function(code){ console.log('MC exited with code '+code); });
   console.log('MC has been spawned');
+
+  // spawn the cloud server
+  var ui = spawn(node5, ['cloud_server.js'], { cwd: __dirname + '/yeti/' });
+  ui.stdout.on('data', function(data){ log('cloud_server stdout', data); });
+  ui.stderr.on('data', function(data){ log('cloud_server stderr', data); });
+  ui.on('exit', function(code){ console.log('cloud_server exited with code '+code); });
+  console.log('cloud_server has been spawned');
 }
