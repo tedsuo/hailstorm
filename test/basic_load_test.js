cf1ef00a91cb3bd62b602745d0545fa22733a683
tedsuo <ted@radicaldesigns.org>
2011-09-11 18:33:39 -0700

    moving things around

--- test/basic_load_test.js
+++ test/basic_load_test.js
@@ -1,3 +1,9 @@
 // Assumes both yeti and hailstorm are running and connected
 
 var http = require('http');
+var mc = require('../mc/client');
+var util = require('util');
+var assert = require('assert');
+
+mc_client = mc.createClient();
+console.log(util.inspect(mc_client));
